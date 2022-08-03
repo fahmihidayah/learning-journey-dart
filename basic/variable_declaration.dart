5baae42adea729baceb80b30b9eac2deb2bcc915
Muhammad Fahmi Hidayah <vambex.system@gmail.com>
2022-08-03 11:23:45 +0700

    first commit

---
+++ basic/variable_declaration.dart
@@ -0,0 +1,15 @@
+void main() {
+  int age = 10;
+  var otherAge = 10;
+
+  double price = 10.0;
+  var otherPrice = 12.0;
+
+  String name = "fahmi";
+  var otherName = "budi";
+
+  // nullable type
+
+  int? unknownAge = 12;
+  unknownAge = null;
+}
